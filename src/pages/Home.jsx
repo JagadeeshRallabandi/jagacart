186b33fb48db186359d4c07b8645fbc17656ee8c
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2025-06-13 16:30:43 +0530

    MainBanner_1_08

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner'
+
+const Home = () => {
+  return (
+    <div claasName='mt-10'>
+        <MainBanner/>
+    </div>
+  )
+}
+
+export default Home
