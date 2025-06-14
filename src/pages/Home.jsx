795d5f964bfeba8189582d795ed0dc9fc43be9e7
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2025-06-15 00:05:07 +0530

    Categories_1_19

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MainBanner from '../components/MainBanner'
+import Categories from '../components/Categories'
 
 const Home = () => {
   return (
     <div claasName='mt-10'>
         <MainBanner/>
+        <Categories/>
     </div>
   )
 }
