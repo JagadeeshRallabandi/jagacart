41c3b001450a46a75c25a9db556053b705d09b54
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2025-06-15 23:11:19 +0530

    Cart_3_59

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -58,9 +58,9 @@ export const AppContextProvider = ({ children }) => {
   const getCartAmount = () =>{
     let totalAmount = 0;
     for(const item in cartItems){
-       let itemInfo = products.find((product)=>product._id ===items);
-       if(cartItems[items]>0){
-          totalAmount += itemInfo.offerPrice * cartItems[items];
+       let itemInfo = products.find((product)=>product._id ===item);
+       if(cartItems[item]>0){
+          totalAmount += itemInfo.offerPrice * cartItems[item];
        }
       }
       return Math.floor(totalAmount * 100) / 100; // Round to two decimal places
