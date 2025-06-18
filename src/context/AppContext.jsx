38bb311bf9dcc2df7792df501a0bcd1aa68ba837
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2025-06-18 21:45:24 +0530

    Your commit message here

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -117,6 +117,25 @@ export const AppContextProvider = ({ children }) => {
     fetchProducts();
     
   }, []);
+
+  // Update Database Cart Items
+useEffect(()=>{
+    const updateCart = async() =>{
+      try {
+        const { data } =  await axios.post('/api/cart/update',{cartItems})
+        if(!data.success){
+          toast.error(data.message)
+        }
+      } catch (error) {
+        toast.error(error.message)
+      }
+    }
+    if(user){
+      updateCart();
+    }
+
+}, [cartItems])
+
   const value = {
     navigate,
     user,
